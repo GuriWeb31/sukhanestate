aba4f4841beb43ffdd2ea2c9ea36ce41b0e709f0
unknown <guribwi123@gmail.com>
2025-04-24 16:35:31 +0530

    code updated

--- src/Worldclass/index.jsx
+++ src/Worldclass/index.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Wordclass = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+    <section className='world-class'>
+
+    </section>
+    
+    </>
   )
 }
 
